22477e650d1344b9e5a5760f76c8cb9d041087cb
tlswogus1199 <giovanni1199@naver.com>
2024-01-04 17:11:23 +0900

    init

---
+++ lib/component/custom_video_player.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
+import 'package:untitled/component/custom_icon_butten.dart'; // 수정된 import 문
+
+import 'dart:io';
+
+class CustomVideoPlayer extends StatefulWidget {
+  final XFile video;
+  final GestureTapCallback onNewVideoPressed;
+
+  const CustomVideoPlayer({
+    required this.video,
+    required this.onNewVideoPressed,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<CustomVideoPlayer> createState() => _CustomVideoPlayerState();
+}
+
+class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
+  VideoPlayerController? videoController;
+
+  @override
+  void didUpdateWidget(covariant CustomVideoPlayer oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (oldWidget.video.path != widget.video.path) {
+      print(widget.video.path);
+      initializeController();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    initializeController();
+  }
+
+  initializeController() async {
+    final videoController = VideoPlayerController.file(
+      File(widget.video.path),
+    );
+
+    await videoController.initialize();
+
+    videoController.addListener(videoControllerListener);
+
+    setState(() {
+      this.videoController = videoController;
+    });
+  }
+
+  void videoControllerListener() {
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    videoController?.removeListener(videoControllerListener);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (videoController == null) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    return AspectRatio(
+      aspectRatio: videoController!.value.aspectRatio,
+      child: Stack(
+        children: [
+          VideoPlayer(
+            videoController!,
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            left: 0,
+            child: Slider(
+              onChanged: (double val) {
+                videoController!.seekTo(
+                  Duration(seconds: val.toInt()),
+                );
+              },
+              value: videoController!.value.position.inSeconds.toDouble(),
+              min: 0,
+              max: videoController!.value.duration.inSeconds.toDouble(),
+            ),
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: CustomIconButton(
+              onPressed: widget.onNewVideoPressed,
+              iconData: Icons.photo_camera_back,
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly, // 수정된 MainAxisgnment
+              children: [
+                CustomIconButton(
+                  onPressed: onReversePressed,
+                  iconData: Icons.rotate_left,
+                ),
+                CustomIconButton(
+                  onPressed: onPlayPressed,
+                  iconData: videoController!.value.isPlaying
+                      ? Icons.pause
+                      : Icons.play_arrow,
+                ),
+                CustomIconButton(
+                  onPressed: onForwardPressed,
+                  iconData: Icons.rotate_right,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void onReversePressed() {
+    final currentPosition = videoController!.value.position;
+
+    Duration position = Duration();
+
+    if (currentPosition.inSeconds > 3) {
+      position = currentPosition - Duration(seconds: 3);
+    }
+
+    videoController!.seekTo(position);
+  }
+
+  void onForwardPressed() {
+    final maxPosition = videoController!.value.duration;
+    final currentPosition = videoController!.value.position;
+
+    Duration position = maxPosition;
+
+    if ((maxPosition - Duration(seconds: 3)).inSeconds >
+        currentPosition.inSeconds) {
+      position = currentPosition + Duration(seconds: 3);
+    }
+    videoController!.seekTo(position);
+  }
+
+  void onPlayPressed() {
+    if (videoController!.value.isPlaying) {
+      videoController!.pause();
+    } else {
+      videoController!.play();
+    }
+  }
+}
